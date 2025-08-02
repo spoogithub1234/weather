1875981d1ff64ad615431d58125336c6c6677fce
spoogithub1234 <spoorthim768@gmail.com>
2025-08-02 17:24:47 +0530

    allowd host *

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i!h#yaim8w3hmvpnfmhmmji_xi!-_u@+@^q(e!vo)jo77-g415
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
